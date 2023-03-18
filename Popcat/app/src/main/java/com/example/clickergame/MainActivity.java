39cc97f2640982d4ffd7c6f96f72cd51f5ec027d
haeun <haeun3882@naver.com>
2023-03-18 22:29:47 +0900

    [ADD] 안드로이드 분야 체험 popcat

---
+++ Popcat/app/src/main/java/com/example/clickergame/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.clickergame;
+
+import static android.view.KeyEvent.ACTION_UP;
+import static android.view.MotionEvent.ACTION_BUTTON_PRESS;
+import static android.view.MotionEvent.ACTION_DOWN;
+import static android.view.MotionEvent.ACTION_HOVER_MOVE;
+import static android.view.MotionEvent.ACTION_MOVE;
+import static android.view.MotionEvent.ACTION_POINTER_DOWN;
+import static com.example.clickergame.R.drawable.popcat_1;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgPopCat;
+    TextView result;
+    int sum;
+    Button reset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgPopCat = findViewById(R.id.img_cat);
+        result = findViewById(R.id.tv_result);
+        reset = findViewById(R.id.bt_reset);
+
+        imgPopCat.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case ACTION_DOWN: {
+                        imgPopCat.setImageResource(R.drawable.popcat_2);
+                        sum = sum + 1;
+                        result.setText(String.valueOf(sum));
+
+                        break;
+                    } case ACTION_UP: {
+                        imgPopCat.setImageResource(popcat_1);
+
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
+
+        reset.setOnClickListener(view -> {
+                    sum = 0;
+                    result.setText(String.valueOf(sum));
+                    imgPopCat.setImageResource(popcat_1);
+                }
+        );
+    }
+}
